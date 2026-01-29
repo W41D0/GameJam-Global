ae25308a8ff7e903a711d2d1e70e16829d79b06e
W41D0 <walidogamer48@gmail.com>
2026-01-29 19:39:37 +0300

    e

---
+++ Assets/Scripts/Sheep Escape Manager.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepEscapeManager : MonoBehaviour
+{
+    [SerializeField] int maxEscapedSheep = 3;
+    [SerializeField] float minTimeToEscape = 2f;
+    [SerializeField] float maxTimeToEscape = 5f;
+    [SerializeField] GameManager gameManager;
+    
+
+    public List<GameObject> sheepList = new List<GameObject>();
+    public List<GameObject> livingSheepList = new List<GameObject>();
+    public static SheepEscapeManager Instance;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        livingSheepList = new List<GameObject>(sheepList);
+        StartCoroutine(escapeSheep());
+    }
+
+    void Update()
+    {
+        if (livingSheepList.Count <= 0)
+        {
+            gameManager.ResetGame();
+        }
+    }
+
+
+    public void addSheepToList(GameObject sheep)
+    {
+        sheepList.Add(sheep);
+        SheepBehaviour behaviour = sheep.GetComponent<SheepBehaviour>();
+        behaviour.setIsEscaped(false);
+    }
+
+    public void removeSheepFromList(GameObject sheep)
+    {
+        if (sheepList.Contains(sheep))
+        {
+            sheepList.Remove(sheep);
+        }
+    }
+
+    public GameObject GetRandomSheep()
+    {
+        if (sheepList.Count > 0)
+        {
+            int randomSheep = Random.Range(0, sheepList.Count);
+            return sheepList[randomSheep];
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+IEnumerator escapeSheep()
+{
+    while (true)
+    {
+        yield return new WaitUntil(() => SheepBehaviour.numOfEscaped < maxEscapedSheep);
+        yield return new WaitForSeconds(Random.Range(minTimeToEscape, maxTimeToEscape));
+        
+        if (sheepList.Count > 0) 
+        {
+            GameObject candidateSheep = GetRandomSheep();
+            if (candidateSheep != null)
+            {
+                SheepBehaviour behaviour = candidateSheep.GetComponent<SheepBehaviour>();
+                if (behaviour != null)
+                {
+                    if (behaviour != null)
+                    {
+                        behaviour.EscapePen();
+                    }
+                }
+            }
+        }
+        else
+        {
+            yield return null;
+        }
+    }
+}
+
+    bool isSheepEscaped(GameObject sheep)
+    {
+        SheepBehaviour behaviour = sheep.GetComponent<SheepBehaviour>();
+        return behaviour.getIsEscape();
+    }
+}
---
+++ Assets/Scripts/Sheep Spawn.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using UnityEngine;
+
+public class SheepSpawn : MonoBehaviour
+{
+    [SerializeField] GameObject sheep;
+    [SerializeField] float spawnTime1 = 2f;
+    [SerializeField] float spawnTime2 = 5f;
+    [SerializeField] int numOfSheep;
+    
+    SheepEscapeManager escapeManager;
+    GameObject Perimeter;
+
+    float randomSpawnTime;
+    Transform pos1;
+    Transform pos2;
+    
+    void Start()
+    {
+        //takes the positions if the 2 children as a reference point
+        pos1 = transform.GetChild(0);
+        pos2 = transform.GetChild(1);
+
+        Perimeter = GameObject.FindGameObjectWithTag("Perimeter");
+        escapeManager = Perimeter.GetComponent<SheepEscapeManager>();
+
+        SpawnScript(numOfSheep); //starts the spawning loop
+    }
+
+    //method to spawn sheep
+    void SpawnScript(int numOfSheeps)
+    {
+        for(int i = 0; i < numOfSheeps; i++)
+        {
+            //sets the spawnTime of the next sheep
+            randomSpawnTime = Random.Range(spawnTime1, spawnTime2);
+
+            //sets the spawn location of the sheep
+            float posX = Random.Range(pos1.position.x, pos2.position.x);
+            float posY = Random.Range(pos1.position.y, pos2.position.y);
+            Vector2 spawnPosition = new Vector2(posX, posY);
+
+            GameObject newSheep = Instantiate(sheep, spawnPosition, Quaternion.Euler(0, 0, 0)); //spawns the sheep
+            escapeManager.addSheepToList(newSheep);
+        }
+    }
+}
