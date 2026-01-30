af9044867afbb5edbe92e124033ff4623186dc66
W41D0 <walidogamer48@gmail.com>
2026-01-31 02:19:48 +0300

    gave checkboard traits reference

--- Assets/Scripts/Assasin Atributes Checklist.cs
+++ Assets/Scripts/Assasin Atributes Checklist.cs
@@ -1,15 +1,30 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class AssasinAtributesChecklist : MonoBehaviour
 {
+    int gender;
+    SpriteRenderer bodyType;
+    SpriteRenderer clothesStyle;
+    SpriteRenderer hairStyle;
+
+    [Header("Gender Nuetral")]
+    [SerializeField] List<Sprite> maskSprites;
+    [SerializeField] List<Sprite> bodyTypeList;
+    [SerializeField] List<Color> clothesColorList;
+    [SerializeField] List<Color> hairColorList;
+
+    [Header("Male Specific")]
+    [SerializeField] List<Sprite> maleClothesList;
+    [SerializeField] List<Sprite> maleHairStyles;
+
+    [Header("Female Specific")]
+    [SerializeField] List<Sprite> femaleClothesList;
+    [SerializeField] List<Sprite> femaleHairStyles;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
     {
         
     }
